9c4197c87e382a7fe4b4d470231aaa9f5305cf66
Ben Emery <ben.s.emery@gmail.com>
2015-01-17 18:03:11 +0000

    Update setup to run tests + include pytest requirement

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from distutils.core import setup, Command
 
 from filewatch import VERSION
+
+# you can also import from setuptools
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
 
 with open('README.rst', 'rb') as fin:
     README = fin.read()
 
 PACKAGE_NAME = 'filewatch'
 
+tests_require = [
+    'pytest',
+]
+
 setup(name=PACKAGE_NAME,
       version=VERSION,
+      cmdclass = {'test': PyTest},
       description='Python File Watcher',
       package_data={PACKAGE_NAME: ['{}.ini'.format(PACKAGE_NAME), ], },
       long_description=README,
@@ -17,4 +38,7 @@ setup(name=PACKAGE_NAME,
       url='https://github.com/benemery/%s' % PACKAGE_NAME,
       download_url='https://github.com/benemery/%s/tarball/%s' % (VERSION, PACKAGE_NAME),
       packages=[PACKAGE_NAME, ],
+      extras_require={
+        'tests': tests_require,
+    },
      )
