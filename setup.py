270a8c373ddb904215dd120972a191fa54482a2d
Ben Emery <ben.s.emery@gmail.com>
2015-01-17 12:46:01 +0000

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='filewatch',
+      version='0.0.1',
+      description='Python File Watcher',
+      author='Ben Emery',
+      url='https://github.com/benemery/filewatch',
+      packages=['filewatch', ],
+     )
