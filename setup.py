936505ae0bc676708a94f96fab0705acbee1b718
Ben Emery <ben.s.emery@gmail.com>
2015-01-17 16:47:23 +0000

    Add long descripton

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from distutils.core import setup
 
 from filewatch import VERSION
 
+with open('README.rst', 'rb') as fin:
+    README = fin.read()
+
 setup(name='filewatch',
       version=VERSION,
       description='Python File Watcher',
+      long_description=README,
       author='Ben Emery',
       url='https://github.com/benemery/filewatch',
       download_url='https://github.com/benemery/filewatch/tarball/%s' % VERSION,
