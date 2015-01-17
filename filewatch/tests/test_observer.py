bc254ce455cf149e1fb645fa0b66612b65f6ca99
Ben Emery <ben.s.emery@gmail.com>
2015-01-17 18:25:27 +0000

    Improve observer testing

--- filewatch/tests/test_observer.py
+++ filewatch/tests/test_observer.py
@@ -16,13 +16,52 @@ class ObserverObj3(ObserverBase):
         a_list = kwargs['a_list']
         a_list.append('c')
 
-class MySubject(Subject):
-    pass
-
 class TestObserver(object):
+    def test_duplicate_observers(self):
+        """Can we add duplicate observers?"""
+        subject = Subject()
+
+        ob1 = ObserverObj1()
+        subject.register_observer(ob1)
+        subject.register_observer(ob1)
+
+        assert len(subject.observers) == 1
+
+    def test_remove_removed_observer(self):
+        """Can we remove the same observer twice?"""
+        subject = Subject()
+
+        ob1 = ObserverObj1()
+        subject.register_observer(ob1)
+
+        assert len(subject.observers) == 1
+
+        subject.remove_observer(ob1)
+        assert len(subject.observers) == 0
+
+        subject.remove_observer(ob1)
+        assert len(subject.observers) == 0
+
+    def test_observer_add_remove(self):
+        """Can we add and remove observers?"""
+        subject = Subject()
+
+        ob1 = ObserverObj1()
+        subject.register_observer(ob1)
+        subject.register_observer(ObserverObj2())
+        subject.register_observer(ObserverObj3())
+
+        assert len(subject.observers) == 3
+
+        subject.remove_observer(ob1)
+        assert len(subject.observers) == 2
+
+        subject.remove_observers()
+        assert len(subject.observers) == 0
+
     def test_notifications(self):
         """Does a subject broadcast to all observers?"""
-        subject = MySubject()
+        subject = Subject()
 
         subject.register_observer(ObserverObj1())
         subject.register_observer(ObserverObj2())
