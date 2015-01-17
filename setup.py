0f000b0f9359a209557db1cf11ff103961179be0
Ben Emery <ben.s.emery@gmail.com>
2015-01-17 17:51:08 +0000

    Ensure config gile is included in package

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ from filewatch import VERSION
 with open('README.rst', 'rb') as fin:
     README = fin.read()
 
-setup(name='filewatch',
+PACKAGE_NAME = 'filewatch'
+
+setup(name=PACKAGE_NAME,
       version=VERSION,
       description='Python File Watcher',
+      package_data={PACKAGE_NAME: ['{}.ini'.format(PACKAGE_NAME), ], },
       long_description=README,
       author='Ben Emery',
-      url='https://github.com/benemery/filewatch',
-      download_url='https://github.com/benemery/filewatch/tarball/%s' % VERSION,
-      packages=['filewatch', ],
+      url='https://github.com/benemery/%s' % PACKAGE_NAME,
+      download_url='https://github.com/benemery/%s/tarball/%s' % (VERSION, PACKAGE_NAME),
+      packages=[PACKAGE_NAME, ],
      )
